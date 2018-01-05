439b5c5e792fd9c2ad6f0ea44ed44fbeecb03cee
Manali Joshi <manalijoshi82@gmail.com>
2018-01-04 22:56:15 -0800

    randomly retweets a tweet from #javascript

--- bot.js
+++ bot.js
@@ -1,25 +1,65 @@
+
+
 console.log("The bot is starting II");
 
 var Twit = require ('twit');
 
+//importing config.js file
 var config = require('./config');
-//console.log(config);
 
+//make a new Twit object
 var T = new Twit(config);
 
-var params ={
-    screen_name : "manali_joshee"
-}
+var stream = T.stream('statuses/filter', { track: '#javascript' })
 
-T.get('users/show', params, gotData);
+var allTweets= [];
+stream.on('tweet', function (tweet) {
+    if(tweet.retweeted == false){
+        //console.log(tweet.id_str);
+        allTweets.push(tweet.text);
+    }
+})
+    setInterval(tweetIt, 1000 * 60 * 20);
+    function tweetIt(){
+        var randomIndex = Math.round(Math.random() * allTweets.length);
+        //console.log(allTweets[randomIndex]);
+        T.post('statuses/retweet/:id', { id: 'allTweets[randomIndex]' }, function (err, tweet, response) {
+            if(err){
+                console.log(error);
+            }
+      
+            console.log(tweetIt.text);
+        });
+      
+  }
+
+
+  
+
+
+/*
+T.stream('statuses/filter', {track: ''}, function(stream) {
+    stream.on('data', function(tweet){
+        console.log(tweet.text);
+    });
+
+    stream.on('error', function(error){
+        console.log(error);
+    });
+});
+*/
+
+
+//T.get('geo/reverse_geocode', params, gotData);
 //T.get('statuses/mentions_timeline', params, gotData);
 
-
+/*
 function gotData(err, data, response) {
     //var tweets = data.statuses;
-
+    for(var i = 0; i < data.length; i++){
         console.log(data);
-  
+    }
+       
         /*
         T.post('favorites/create', {id: data[i].id_str}, function(e,d,r){
             if (e) {
@@ -28,6 +68,6 @@ function gotData(err, data, response) {
                 console.log("Liked " + d.id );
             }
         });
-        */
+        
     }
-  
+  */
